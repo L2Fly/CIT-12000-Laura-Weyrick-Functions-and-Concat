97d33dbbd24ca158d40a76a7b53046b8cb8df845
Laura Weyrick <65510690+L2Fly@users.noreply.github.com>
2020-05-20 13:56:56 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var laura = prompt("What is your name?");
+var candy = prompt("What is your favorite candy?");
+var lynette = prompt("What is your name?");               
+var bonbon =prompt("What is your least favorite candy?");       
+                      
+function writeHTML(words,words2){ 
+    
+  document.write(" My name is "+laura+" and my favorite candy are "+candy+".");
+  document.write(" My name is "+lynette+" and my least favorite candy are "+bonbon+".");
+  
+}
+
+writeHTML("words","words2");
+writeHTML("words","words2");
